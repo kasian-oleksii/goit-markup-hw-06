74af6a718e24b65bbe335d6ade5fdde8263c6ca0
OleksiiKasian <kasianalex1@gmail.com>
2025-02-10 21:56:39 +0200

    add initial files

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,11 @@
 (() => {
   const refs = {
     // Додати атрибут data-modal-open на кнопку відкриття
-    openModalBtn: document.querySelector("[data-menu-open]"),
+    openModalBtn: document.querySelector("[data-modal-open]"),
     // Додати атрибут data-modal-close на кнопку закриття
-    closeModalBtn: document.querySelector("[data-menu-close]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
     // Додати атрибут data-modal на бекдроп модалки
-    modal: document.querySelector("[data-menu]"),
+    modal: document.querySelector("[data-modal]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
